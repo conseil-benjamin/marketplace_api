ae005cc91b68d4fd17c71a622a3ff2c944e0e992
conseil-benjamin <benlenormand61@gmail.com>
2023-12-14 08:35:57 +0100

    add routes for post and delete a favorite product

--- routes/favoris.routes.js
+++ routes/favoris.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { getFavoris } = require("../controllers/favoris.controller");
+const { getFavoris, postFavori, deleteFavori } = require("../controllers/favoris.controller");
 const router = express.Router();
 
 router.get("/:idClient", getFavoris);
+router.post("/insert", postFavori);
+router.post("/:idProduct", deleteFavori);
 
 module.exports = router;
