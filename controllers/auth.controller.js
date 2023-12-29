dd0bc7a6e2d2dff6649bf9d3d03b7748ecc5170d
conseil-benjamin <benlenormand61@gmail.com>
2023-12-29 21:38:57 +0100

    try to solve issue with token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,31 +5,32 @@ const crypto = require("crypto");
 
 module.exports.login = async (req, res) => {
   try {
-    let userInformations = req.body;
+    const userInformations = req.body;
     const email = userInformations.email;
     const password = userInformations.password;
 
-    let user = UsersModel.find({ adresseEmail: email });
+    const user = await UsersModel.findOne({ adresseEmail: email });
     if (!user) {
-      throw new Error("Invalid email or password");
+      return res.status(401).json({ message: "Invalid email or password" });
     }
 
     try {
       const isSamePassword = await bcrypt.compare(password, user.mdp);
       if (!isSamePassword) {
-        throw new Error("Invalid email or password");
+        return res.status(401).json({ message: "Invalid email or password" });
       }
     } catch (error) {
       console.error("Erreur lors de la comparaison de hachage :", error);
-      throw new Error("Invalid email or password");
+      return res.status(500).json({ message: "Internal Server Error" });
     }
+
     const secretKey = crypto.randomBytes(64).toString("hex");
 
     const token = jwt.sign({ id: user.id }, secretKey, { expiresIn: "7d" });
 
     res.status(200).json({ token });
   } catch (error) {
-    res.status(500);
-    throw new Error(error.message);
+    console.error("Erreur lors de la connexion :", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
