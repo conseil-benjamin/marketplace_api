f60d20a93f07e87c5f44b160905cea7f70fe4bc7
conseil-benjamin <benlenormand61@gmail.com>
2023-12-30 18:17:15 +0100

    adding a readme for docker build and run and adding a new env key for jwt token verification

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,36 +1,34 @@
 const UsersModel = require("../models/users.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const crypto = require("crypto");
 
 module.exports.login = async (req, res) => {
   try {
-    const userInformations = req.body;
+    let userInformations = req.body;
     const email = userInformations.email;
     const password = userInformations.password;
 
-    const user = await UsersModel.findOne({ adresseEmail: email });
+    // Vérifiez d'abord si l'email est associé à un compte
+    let user = await UsersModel.findOne({ adresseEmail: email });
     if (!user) {
-      return res.status(401).json({ message: "Invalid email or password" });
+      throw new Error("Invalid email or password");
     }
 
-    try {
-      const isSamePassword = await bcrypt.compare(password, user.mdp);
-      if (!isSamePassword) {
-        return res.status(401).json({ message: "Invalid email or password" });
-      }
-    } catch (error) {
-      console.error("Erreur lors de la comparaison de hachage :", error);
-      return res.status(500).json({ message: "Internal Server Error" });
+    const isSamePassword = await bcrypt.compare(password, user.mdp); 
+    if (!isSamePassword) {
+      throw new Error("Invalid email or password");
     }
 
-    const secretKey = crypto.randomBytes(64).toString("hex");
-
-    const token = jwt.sign({ id: user.id }, secretKey, { expiresIn: "7d" });
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.CLE_SECRETE,
+      { expiresIn: "7d" }
+    );
 
     res.status(200).json({ token });
   } catch (error) {
-    console.error("Erreur lors de la connexion :", error);
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(500).json({ error: error.message });
   }
 };
