88210a8dee8c92ba49f7519e4bc0fa126f616c66
conseil-benjamin <benlenormand61@gmail.com>
2024-01-28 15:36:25 +0100

    change return value of favoris.controller.js delete method, now return the list of favoris minus the one deleted AND create route insert for basket table

--- controllers/panier.controller.js
+++ controllers/panier.controller.js
@@ -1,8 +1,8 @@
 const Paniers = require("../models/panier.model");
+const Favoris = require("../models/favoris.model");
 
 module.exports.getPanier = async (req, res) => {
     try {
-        console.log("dadada");
         const numClient = req.userId;
         const panier = await Paniers.findOne({ numeroClient: numClient });
 
@@ -14,5 +14,28 @@ module.exports.getPanier = async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Erreur serveur" });
+    }
+};
+
+module.exports.insertPanier = async (req, res) => {
+    try {
+        const produit = req.body.panierInfos;
+        console.log(produit);
+        const id = req.userId;
+        const timestamp = new Date().getTime();
+        const uniqueId = `${timestamp}`;
+        let panier = await Paniers.findOne({ numeroClient: id });
+        if (!panier) {
+            panier = await Paniers.create({ numeroPanier: uniqueId ,numeroClient: id, contenuPanier: produit });
+        } else {
+            panier.contenuPanier.push(produit);
+        }
+
+        const result = await panier.save();
+
+        res.json({ success: true, panier: result });
+    } catch (error) {
+        console.error("Erreur lors de l'insertion des données:", error);
+        res.status(500).json({ success: false, error: "Erreur serveur" });
     }
 };
--- routes/panier.routes.js
+++ routes/panier.routes.js
@@ -8,6 +8,8 @@ const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.get("/", authMiddleware, getPanier);
-//router.post("/insert", authMiddleware, insertPanier);
-//router.delete("/delete", authMiddleware, deleteProductFromPanier);
+router.post("/insert", authMiddleware, insertPanier);
+//router.post("/insert-many-products", authMiddleware, insertManyProducts); // insert many products
+//router.delete("/delete", authMiddleware, deleteProductFromPanier); // delete product from panier
+//router.patch("/update", authMiddleware, updatePanier); // update product number for example
 module.exports = router;
