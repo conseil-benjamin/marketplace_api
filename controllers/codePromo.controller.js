25f2a2e2f843d7ec76dbf3fc3eea81927e56315b
conseil-benjamin <benlenormand61@gmail.com>
2024-03-15 13:32:03 +0100

    feat: add routes for codePromo.routes.js

--- controllers/codePromo.controller.js
+++ controllers/codePromo.controller.js
@@ -22,3 +22,55 @@ module.exports.getCodePromo = async (req, res) => {
         res.status(500).json({ message: "Erreur serveur" });
     }
 };
+
+module.exports.getCodesPromo = async (req, res) => {
+    try {
+        const result = await CodePromos.find({ });
+
+        if (!result) {
+            res.status(400).json({ message: "Aucun code promo trouvée" });
+            return;
+        }
+        console.log(result);
+        res.status(200).json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur serveur" });
+    }
+};
+
+module.exports.removeCodePromo = async (req, res) => {
+    try {
+        const result = await CodePromos.deleteOne({_id: req.params.id});
+
+        if (!result) {
+            res.status(400).json({ message: "Aucun code promo trouvée avec cet id" });
+            return;
+        }
+
+        const allCodePromos = await CodePromos.find({});
+        res.status(200).json(allCodePromos);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur serveur" });
+    }
+};
+
+module.exports.addCodePromo = async (req, res) => {
+    try {
+        const codePromo = req.body;
+        console.log(codePromo);
+        const result = await CodePromos.insertMany(codePromo);
+        if (!result) {
+            res.status(400).json({ message: "Aucun code promo trouvée avec cet id" });
+            return;
+        }
+        const allCodePromos = await CodePromos.find({});
+
+        console.log(result);
+        res.status(200).json(allCodePromos);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur serveur" });
+    }
+};
--- routes/codePromo.routes.js
+++ routes/codePromo.routes.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const { getCodePromo} = require("../controllers/codePromo.controller");
+const { getCodePromo, getCodesPromo, removeCodePromo, addCodePromo} = require("../controllers/codePromo.controller");
 const router = express.Router();
 
+router.get("/", getCodesPromo);
+router.post("/add", addCodePromo);
 router.get("/:codePromo", getCodePromo);
+router.delete("/remove/:id", removeCodePromo);
 module.exports = router;
