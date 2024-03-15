be23eb235ec4287d96c7c4bd0fab6a95fb4fa401
conseil-benjamin <benlenormand61@gmail.com>
2024-03-15 13:33:27 +0100

    feat: add a route for commandes.routes.js to fetch all orders on the site.

--- routes/commandes.routes.js
+++ routes/commandes.routes.js
@@ -2,11 +2,15 @@ const express = require("express");
 const {
   getCommandes,
   getOneOrder,
+  getAllOrders,
+    updateTrackingNumber,
 } = require("../controllers/commandes.controller");
 const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.get("/", authMiddleware, getCommandes);
+router.get("/getAllOrders", getAllOrders);
+router.patch("/update-tracking-number", updateTrackingNumber);
 router.get("/:idCommande", authMiddleware, getOneOrder);
 
 module.exports = router;
