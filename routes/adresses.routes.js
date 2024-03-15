496cabccac925fce0cced3f292d089d90ac71943
conseil-benjamin <benlenormand61@gmail.com>
2024-03-15 13:38:04 +0100

    feat: add a route to fetch a client adress by his objectID.

--- routes/adresses.routes.js
+++ routes/adresses.routes.js
@@ -4,8 +4,8 @@ const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.get("/", authMiddleware, getAdresses);
-router.get("/:adresseId", authMiddleware, getAdresseByObjectId);
 router.post("/insert", authMiddleware, insertAdresse);
 router.delete("/delete", authMiddleware, deleteAdresse);
 router.patch("/update", authMiddleware, updateAdresse);
+router.get("/:adresseId", authMiddleware, getAdresseByObjectId);
 module.exports = router;
